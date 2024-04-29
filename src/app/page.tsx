d7bce1845ae721d2d458837842c7ba546d3706ca
544-eth <elioku20@gmail.com>
2024-04-29 17:50:27 +0100

    started

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=' w-full h-full flex justify-center items-center relative bg-black'>
+        <div className=' max-w-screen-lg w-full h-full flex relative'>
+          <section className=' fixed w-[275px] flex flex-col h-screen text-white'>
+            
+          </section>
+          <main className=' text-white'>Home Timeline</main>
+          <section className=' text-white'>Right side</section>
+        </div>
+    </div>
+  )
+}
+
+export default Home
